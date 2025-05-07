0654794b35474b8c92efe0c355878c609a09d16c
Naftaly Weinberger <naftaly@inventoryahead.com>
2025-05-06 23:56:36 -0400

    Add Microsoft.ApplicationInsights.AspNetCore package for telemetry and create /error endpoint with 500 status

--- src/Azure-demo-app/Azure-demo-app/Program.cs
+++ src/Azure-demo-app/Azure-demo-app/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services if needed
+builder.Services.AddApplicationInsightsTelemetry();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World, from github actions!");
+// Configure the HTTP request pipeline
+app.MapGet("/hello", () => "Hello from Minimal API!");
+
+app.MapGet("/error", () =>
+{
+    // Simulate an internal server error
+    return Results.StatusCode(500);
+});
 
 app.Run();
